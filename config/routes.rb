cf91c61a4235ebf28f1bb67c994e303ebc7737c9
andreasantarl <andrea.santarl@gmail.com>
2019-05-08 19:07:43 -0400

    Changes root to cakes#index, creates partial to render all cakes for homepage if user signed in

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   resources :cakes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
-  root to: 'home#index'
+  root to: 'cakes#index'
 
 end
