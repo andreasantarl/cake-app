8148dfd76a804c1263f4c1b86fc5429d8725cbd4
andreasantarl <andrea.santarl@gmail.com>
2019-05-12 16:58:53 -0400

    Moves around devise menu registration partial render to application view

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   resources :cakes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
+
   devise_for :users, skip: [:sessions]
   as :user do
     get 'admin', to: 'devise/sessions#new', as: :new_user_session
