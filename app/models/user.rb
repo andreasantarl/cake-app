8e2aa35f466c0ddd5313d03f627e648748213bc6
andreasantarl <andrea.santarl@gmail.com>
2019-07-15 21:06:38 -0400

    Generates scaffold for comment, adds relationship to Comment and User models

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :cakes
+  has_many :comments, dependent: :destroy
 
   def assign_default_role
     self.add_role(:user) if self.roles.blank?
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_16_004053) do
+ActiveRecord::Schema.define(version: 2019_07_16_010215) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,13 @@ ActiveRecord::Schema.define(version: 2019_07_16_004053) do
     t.boolean "show_on_homepage", default: false
     t.integer "position"
     t.index ["user_id"], name: "index_cakes_on_user_id"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text "comment"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "roles", force: :cascade do |t|
